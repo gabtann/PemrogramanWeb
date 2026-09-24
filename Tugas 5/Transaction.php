74667691f6b6ada8ec2e78fc00e25f93374a3560
gabtann <gabrieltan306@gmail.com>
2026-09-24 17:45:40 +0800

    Tambah proses transaksi

--- Tugas 5/Transaction.php
+++ Tugas 5/Transaction.php
@@ -9,4 +9,36 @@ class Transaction
         private string $type,
         private float $amount,
     ) {}
+
+    public function process(): bool
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+        }
+
+        if (!isset($_SESSION['balance'])) {
+            $_SESSION['balance'] = 0;
+        }
+
+        return match ($this->type) {
+            'deposit' => $this->processDeposit(),
+            'withdrawal' => $this->processWithdrawal(),
+        };
+    }
+    
+    private function processWithdrawal(): bool
+    {
+        if ($_SESSION['balance'] >= $this->amount) {
+            $_SESSION['balance'] -= $this->amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function processDeposit(): bool
+    {
+        $_SESSION['balance'] += $this->amount;
+        return true;
+    }   
 }
